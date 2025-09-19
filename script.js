89c08ad9cd03e99a4cbf6a16339680b24a75b5c7
Lucy Thomas <greenangela218@gmail.com>
2025-09-19 21:51:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -63,9 +63,18 @@ document.getElementById("closeProduct").onclick=()=>closeOverlay("productOverlay
 // Products & cart
 let cart=[];
 let products=[
-  {id:1,title:"Milano Coat",desc:"Italian tailored wool coat",price:1290,img:"IMG-20250919-WA0002.jpg"},
-  {id:2,title:"Classic Knit",desc:"Soft cotton knitwear",price:180,img:"IMG-20250919-WA0003.jpg"},
-  {id:3,title:"Slim Trousers",desc:"Elegant slim fit",price:240,img:"IMG-20250919-WA0004.jpg"}
+  {id:1,title:"Fitting dresses",desc:"Silk fit",price:290,img:"IMG-1 (1).jpg"},
+  {id:2,title:"Lock Knit",desc:"Lock knitwear",price:180,img:"IMG-1 (2).jpg"},
+  {id:3,title:"Mirrow",desc:"Mirrow",price:240,img:"IMG-1 (3).jpg"},
+  {id:1,title:"Mirrow",desc:"Bead",price:240,img:"IMG-1 (4).jpg"},
+  {id:2,title:"Obele` dress",desc:"Slim fit",price:220,img:"IMG-1 (5).jpg"},
+  {id:3,title:"Obele` dress",desc:"Elegant slim fit",price:220,img:"IMG-1 (6).jpg"},
+  {id:1,title:"Pink night",desc:"Pink",price:290,img:"IMG-1 (7).jpg"},
+  {id:2,title:"Pink night",desc:"Night",price:290,img:"IMG-1 (8).jpg"},
+  {id:3,title:"Rosie`",desc:"Rose`",price:300,img:"IMG-1 (9).jpg"},
+  {id:1,title:"Rosie`",desc:"Bow",price:300,img:"IMG-1 (10).jpg"},
+  {id:2,title:"Rosie`",desc:"Pose bow`",price:300,img:"IMG-1 (11).jpg"},
+  {id:3,title:"Mirrow",desc:"Elegant fit",price:240,img:"IMG-1 (12).jpg"}
 ];
 const productGrid=document.getElementById("productGrid");
 function renderProducts(list){
